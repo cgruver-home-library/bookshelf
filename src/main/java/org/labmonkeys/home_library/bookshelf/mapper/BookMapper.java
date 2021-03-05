80c16b78245f2b92572bb266bc25d126e6cf1108
Charro Gruver <charro_gruver@yahoo.com>
2021-03-05 09:43:24 -0500

    wip

--- src/main/java/org/labmonkeys/home_library/bookshelf/mapper/BookMapper.java
+++ src/main/java/org/labmonkeys/home_library/bookshelf/mapper/BookMapper.java
@@ -3,7 +3,6 @@ package org.labmonkeys.home_library.bookshelf.mapper;
 import java.util.List;
 
 import org.labmonkeys.home_library.bookshelf.dto.BookDTO;
-import org.labmonkeys.home_library.bookshelf.messaging.BookEvent;
 import org.labmonkeys.home_library.bookshelf.messaging.BookState;
 import org.labmonkeys.home_library.bookshelf.model.Book;
 import org.mapstruct.Mapper;
